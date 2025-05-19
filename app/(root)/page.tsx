a403577421c47cca79abe06bdd6f3263e81b4281
LADHU .S <sathasivamladhushaan@gmail.com>
2025-05-19 14:04:08 +0530

    Create Feedback

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,12 +11,12 @@ const Page = async () => {
     const user = await getCurrentUser();
 
     const [userInterviews, latestInterviews] = await Promise.all([
-        await getInterviewsByUserId(user?.id!),
-        await getLatestInterviews({ userId: user?.id! })
+        await getInterviewsByUserId(user?.id!) ?? [],
+        await getLatestInterviews({ userId: user?.id! }) ?? []
     ])
 
-    const hasPastInterviews = userInterviews?.length > 0;
-    const hasUpcomingInterviews = latestInterviews?.length > 0;
+    const hasPastInterviews = userInterviews.length > 0;
+    const hasUpcomingInterviews = latestInterviews.length > 0;
 
     return (
         <>
